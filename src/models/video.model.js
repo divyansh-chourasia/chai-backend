b94f25588e6e65ad4dac7a894b37029ee33c99b2
Divyansh Chourasia <Divyansh Chourasia@LAPTOP-ACKKCLSS.local>
2024-09-14 21:26:43 +0530

    add user and video model

---
+++ src/models/video.model.js
@@ -0,0 +1,41 @@
+import mongoose, { Schema } from "mongoose";
+
+const videoSchema = new Schema(
+  {
+    videoFile: {
+      type: String, // cloudinary url
+      required: true,
+    },
+    thumbnail: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    duration: {
+        type: Number,
+        default: true,
+      },
+    view: {
+      type: Number,
+      default: 0,
+    },
+    isPublished: {
+      type: Boolean,
+      default: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamp: true }
+);
+
+export const Video = new mongoose.model("Video", videoSchema);
