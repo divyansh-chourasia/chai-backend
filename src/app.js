819859d2dacb112d05ee9d47f6fe810436813699
Divyansh Chourasia <Divyansh Chourasia@LAPTOP-ACKKCLSS.local>
2024-09-14 18:32:25 +0530

    util discussion code

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+app.use(express.json({ limit: "16kb" })); //accept json data of limit 6kb
+app.use(express.urlencoded({ extended: true, limit: "16kb" })); //data from url comes with % or & in place of space
+app.use(express.static("public"));
+app.use(cookieParser( ))
+
+
+
+
+export { app };
--- src/index.js
+++ src/index.js
@@ -2,9 +2,21 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
-dotenv.config({ path: './env' });
+dotenv.config({ path: "./env" });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is runnig on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Mongo DB connnection failed !!! ", err);
+  });
+
+
+
+
 
 //APProach 1
 // (async () => {
